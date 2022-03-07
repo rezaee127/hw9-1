877d0029fe7a750f09ff12dcd08cbcc3269f1a4a
razi <rezaie127@yahoo.com>
2022-03-07 10:13:19 +0330

    add profile fragment

--- app/src/main/java/com/example/netflix/HomeFragment.kt
+++ app/src/main/java/com/example/netflix/HomeFragment.kt
@@ -1,13 +1,20 @@
 package com.example.netflix
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getColor
+import com.example.netflix.databinding.FragmentHomeBinding
+import com.google.android.material.button.MaterialButton
 
 class HomeFragment : Fragment() {
-
+    lateinit var  binding : FragmentHomeBinding
+    var arrayOfFavoriteButtons=ArrayList<Button>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,13 +27,28 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = FragmentHomeBinding.inflate (inflater, container, false)
+
+        return binding.root
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        //return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        arrayOfFavoriteButtons= arrayListOf(binding.button1,binding.button2,binding.button3,
+            binding.button4,binding.button5,binding.button6,binding.button7,binding.button8,
+            binding.button9,binding.button10,binding.button11,binding.button12)
+
+       for (button in arrayOfFavoriteButtons){
+           button.setOnClickListener {
+               (button as MaterialButton).setIconTintResource(R.color.red)
+           }
 
 
+            }
     }
+
 }
