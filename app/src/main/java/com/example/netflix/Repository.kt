9c2fe3dde0deed0b63a0f4a2b065e0338274293d
razi <rezaie127@yahoo.com>
2022-04-29 02:46:33 +0430

    added model & Repository

---
+++ app/src/main/java/com/example/netflix/Repository.kt
@@ -0,0 +1,22 @@
+package com.example.netflix
+
+object Repository {
+
+    var arrayOfVideos= arrayListOf(
+    Video(1,"Film 1",R.drawable.avatar_1),Video(2,"Film 2",R.drawable.avatar_2),
+    Video(3,"Film 3",R.drawable.avatar_3),Video(4,"Film 4",R.drawable.avatar_4),
+    Video(5,"Film 5",R.drawable.avatar_5),Video(6,"Film 6",R.drawable.avatar_6),
+    Video(7,"Film 7",R.drawable.avatar_7),Video(8,"Film 8",R.drawable.avatar_8),
+    Video(9,"Film 9",R.drawable.avatar_9),Video(10,"Film 10",R.drawable.avatar_10),
+    Video(11,"Film 11",R.drawable.avatar_11),Video(12,"Film 12",R.drawable.avatar_12),
+    Video(13,"Film 13",R.drawable.avatar_13),Video(14,"Film 14",R.drawable.avatar_14),
+    Video(15,"Film 15",R.drawable.avatar_15),Video(16,"Film 16",R.drawable.avatar_16),
+    Video(17,"Film 17",R.drawable.avatar_17),Video(18,"Film 18",R.drawable.avatar_18),
+    Video(19,"Film 19",R.drawable.avatar_19),Video(20,"Film 20",R.drawable.avatar_20),
+    Video(21,"Film 21",R.drawable.avatar_21),Video(22,"Film 22",R.drawable.avatar_22),
+    Video(23,"Film 23",R.drawable.avatar_23),Video(24,"Film 24",R.drawable.avatar_24),
+    Video(25,"Film 25",R.drawable.avatar_25),Video(26,"Film 26",R.drawable.avatar_26),
+    Video(27,"Film 27",R.drawable.avatar_27),Video(28,"Film 28",R.drawable.avatar_28),
+    Video(29,"Film 29",R.drawable.avatar_29),Video(30,"Film 30",R.drawable.avatar_30))
+
+}
---
+++ app/src/main/java/com/example/netflix/Video.kt
@@ -0,0 +1,6 @@
+package com.example.netflix
+
+import android.widget.ImageView
+
+data class Video(var id:Int, var title:String,var imageView: Int,var isFavorite:Boolean=false) {
+}
